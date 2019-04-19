fe7564839e23f548584f9e15ee7ab4836527d073
sarath <sarath.kumar2@tatacommunications.com>
2019-04-19 13:34:09 +0530

    product detail page related work

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  currentTab: string = 'ingredients';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  tabChange(tab) {
+    this.currentTab = tab;
+  }
 }
